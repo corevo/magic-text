713a0e533a43a2b5dbecacfdf27891b7f8f04501
TomerBrosh <tomerbrosh7@gmail.com>
2015-11-14 20:33:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var exports = module.exports = {};
 exports.MagicText = function (path) {
-    var tess = require('node-tesseract'); //,
-        //fs = require('fs');
+    var tess = require('node-tesseract'),
+        fs = require('fs');
 
-    //var File = fs.readFileSync(path, 'utf8');
-    tess.process(__dirname + "/" + path, function (err, text) {
+    fs.readFile(path, function (err, data) {
+        if (err) throw err;
+        console.log(data);
+    });
+
+
+    tess.process(__dirname + path, function (err, text) {
         if (err) {
-            console.error(err);
+            console.log(err);
         } else {
             console.log(text);
             return (text);
