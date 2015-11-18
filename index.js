555058bdac90c80328731aa68f4f4b7943feec3f
Tomer Brosh <Tomer Brosh>
2015-11-18 20:53:34 +0200

    work using only tesseract [filename] [destname] -l heb

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var exports = module.exports = {};
+exports.MagicTextL = function (path, lang) {
+    var tess = require('node-tesseract'),
+	options = {
+		l: lang,
+		psm: 0
+	};
+    tess.process(__dirname + "/" + path, options , function (err, text) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(text);
+            return (text);
+        }
+    });
+};
 exports.MagicText = function (path) {
     var tess = require('node-tesseract');
+	
     tess.process(__dirname + "/" + path, function (err, text) {
         if (err) {
             console.log(err);
--- test/test.js
+++ test/test.js
@@ -1,2 +1,8 @@
-var Magic = require("../index.js");
-console.log(Magic.MagicText(process.argv[2]));
+var Magic = require("../index2.js");
+Magic.MagicText(process.argv[2], process.argv[3]);
+
+/* if(process.argv[3]){
+console.log(Magic.MagicTextL(process.argv[2],process.argv[3] ));
+}
+else {console.log(Magic.MagicText(process.argv[2]));
+} */
