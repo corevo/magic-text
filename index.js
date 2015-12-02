d095349a50ae2b9617415b7ffe90659912655638
idan berman <idan berman>
2015-12-02 09:17:15 +0200

    'fixes'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 function getText(path, lang , cb) {
 	/*The function's supposed to be [tesseract path stdout -l lang]*/
 	// if (cb != undefined) {
-    exec('tesseract '+ path + 'stdout -l ' + lang , function (error, stdout, stderr) {
+    exec('tesseract '+ path + ' stdout -l ' + lang , function (error, stdout, stderr) {
 		if(error != null) {
 			console.log("There was an error: " + error)
 		}
