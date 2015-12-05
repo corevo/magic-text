239e44d73742d3f418789ac5c4df911b13616002
Tomer Brosh <Tomer Brosh>
2015-12-06 00:58:24 +0200

    more tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,13 @@
 var Tess = require("../index.js");
-Tess.getText(process.argv[2], process.argv[3], function(err,data){
-	console.log(data);
+Tess.getText("1.jpeg", "heb", function(err,data){
+	console.log("1.jpeg: " + data);
+});
+Tess.getText("2.jpg", "heb", function(err,data){
+	console.log("2.jpg: " + data);
+});
+Tess.getText("3.tif", "heb", function(err,data){
+	console.log("3.tif: " + data);
+});
+Tess.getText("4.jpg", "heb", function(err,data){
+	console.log("4.jpg: " + data);
 });
